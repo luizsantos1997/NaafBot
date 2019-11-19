e75d1b475456c2533753e706ca31d8e3694d1786
Luiz Eduardo de Almeida Santos <educelula@hotmail.com>
2019-11-19 18:47:39 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 new Vue({
-  el: "#app",
-  data: {
-    talks:[
-    {text:'Olá Seja Bem vindo!',
-    isBotAnswer:true}
-    ],
-    newText:''
-  },
-  methods: {
-  
-  AddText(){
-  if(this.newText === '' || 
-  	 this.newText === null){
-     alert("Texto vazio!");
-     return false;
-     }
-    var newTalk = {text:this.newText,
-    isBotAnswer:false};
-  	this.talks.push(newTalk);
-    this.newText = '';
-  }
+    el: "#app",
+    data: {
+      talks:[
+      {text:'Olá Seja Bem vindo!',
+      isBotAnswer:true}
+      ],
+      newText:''
+    },
+    methods: {
     
-  }
-})
+    AddText(){
+    if(this.newText === '' || 
+         this.newText === null){
+       alert("Texto vazio!");
+       return false;
+       }
+      var newTalk = {text:this.newText,
+      isBotAnswer:false};
+        this.talks.push(newTalk);
+      this.newText = '';
+    }
+      
+    }
+  });
